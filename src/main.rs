a6f3af8075ee8b23a5078a945f9b7ec90ffc6308
Pawel Perek <pawel@perek.pro>
2021-04-27 19:38:23 +0300

    Add example program in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![allow(dead_code)]
+#![allow(clippy::upper_case_acronyms)]
 
 mod cpu;
 
 fn main() {
-    print!("{:?}", cpu::opcode::decode_op(0xff));
+    //                        LDA  #C0  TAX  INX  BRK
+    let mut cpu = cpu::CPU::new(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+    cpu.execute();
 }
