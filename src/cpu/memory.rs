d34ce738ba20479f4b07efe82a99029c314f2ff6
PawelPerek <pawel@perek.pro>
2022-10-07 16:32:02 +0200

    Cleanup project

--- src/cpu/memory.rs
+++ src/cpu/memory.rs
@@ -4,48 +4,43 @@ pub const EXPANSION: std::ops::Range<i32> = 0x4020..0x6000;
 pub const SAVE: std::ops::Range<i32> = 0x6000..0x8000;
 pub const PRG_ROM: std::ops::Range<i32> = 0x8000..0x10000;
 
-pub struct Memory {
-    pub data: [u8; 0x10000],
-}
+pub struct Memory([u8; 0x10000]);
 
 impl Memory {
+    pub fn new() -> Memory {
+        Memory([0; 0x10000])
+    }
+
+    pub fn access(&self, address: usize) -> u8 {
+        self.0[address]
+    }
+
     pub fn zp(&self, address: u8, offset: u8) -> u8 {
         let addr: usize = {
-            let mut base: u16 = 0;
-            base += address as u16;
-            base += offset as u16;
-            base &= 0xFF;
+            let base = (address as u16) + (offset as u16) & 0xFF;
             base as usize
         };
 
-        self.data[addr]
+        self.0[addr]
     }
 
     pub fn abs(&self, lsd: u8, msd: u8, offset: u16) -> u8 {
         let addr: usize = {
-            let mut base: u16 = 0;
-            base += msd as u16;
-            base <<= 8;
-            base += lsd as u16;
-            base += offset;
+            let base = (msd as u16) << 8 + (lsd as u16) + offset;
             base as usize
         };
 
-        self.data[addr]
+        self.0[addr]
     }
 
     pub fn ind(&self, lsd: u8, msd: u8, offset: u8) -> u8 {
         let addr_ptr: usize = {
-            let mut base: u16 = 0;
-            base += msd as u16;
-            base <<= 8;
-            base += lsd as u16;
-            base += offset as u16;
+            let base = (msd as u16) << 8 + (lsd as u16) + (offset as u16);
             base as usize
         };
 
-        let addr = self.data[addr_ptr] as usize;
+        let addr = self.0[addr_ptr] as usize;
 
-        self.data[addr]
+        self.0[addr]
     }
 }
